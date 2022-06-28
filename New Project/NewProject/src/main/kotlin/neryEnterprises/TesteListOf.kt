9d91a7bc11aedbf75268e4f3eaf6fb2e97740c92
gabrielnerys <gabe.nery@live.com>
2022-06-28 11:12:40 -0300

    Created TesteListOf file

---
+++ New Project/NewProject/src/main/kotlin/neryEnterprises/TesteListOf.kt
@@ -0,0 +1,26 @@
+package neryEnterprises
+
+fun main() {
+    val gabriel = Employee(name = "Gabriel", salary = 4000.0)
+    val roberto = Employee(name = "Roberto", salary = 2500.0)
+    val miranda = Employee(name = "Miranda", salary = 3300.0)
+
+    val employees = listOf(gabriel, roberto, miranda)
+
+    employees.forEach{ println(it) }
+
+    println("------------")
+
+    println(employees.find { it.name == "Roberto" })
+}
+
+data class Employee(
+    val name: String,
+    val salary: Double
+) {
+    override fun toString(): String =
+        """
+            Name:   $name
+            Salary: $salary
+        """.trimIndent()
+}
