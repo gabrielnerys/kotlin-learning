ad09711670c749a717292b7fcbac31ff2bfdead9
gabrielnerys <gabe.nery@live.com>
2022-06-28 19:04:49 -0300

    Update TesteListOf file

--- New Project/NewProject/src/main/kotlin/neryEnterprises/TesteListOf.kt
+++ New Project/NewProject/src/main/kotlin/neryEnterprises/TesteListOf.kt
@@ -1,9 +1,9 @@
 package neryEnterprises
 
 fun main() {
-    val gabriel = Employee(name = "Gabriel", salary = 4000.0)
-    val roberto = Employee(name = "Roberto", salary = 2500.0)
-    val miranda = Employee(name = "Miranda", salary = 3300.0)
+    val gabriel = Employee(name = "Gabriel", salary = 4000.0, employment = "PJ")
+    val roberto = Employee(name = "Roberto", salary = 2500.0, employment = "CLT")
+    val miranda = Employee(name = "Miranda", salary = 3300.0, employment = "CLT")
 
     val employees = listOf(gabriel, roberto, miranda)
 
@@ -16,11 +16,17 @@ fun main() {
     println("------------")
 
     employees.sortedBy { it.name }.forEach{ println(it) }
+
+    println("------------")
+
+    employees.groupBy { it.employment }.forEach{ println(it) }
+
 }
 
 data class Employee(
     val name: String,
-    val salary: Double
+    val salary: Double,
+    val employment: String
 ) {
     override fun toString(): String =
         """
