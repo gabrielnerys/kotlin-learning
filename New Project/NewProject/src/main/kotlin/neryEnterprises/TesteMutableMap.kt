355ae1c2aa29da22a69845f5e641b6e39bc1da10
gabrielnerys <gabe.nery@live.com>
2022-06-29 11:11:05 -0300

    Created TesteMutableCollections file and EmployeesManager Class

---
+++ New Project/NewProject/src/main/kotlin/neryEnterprises/TesteMutableMap.kt
@@ -0,0 +1,15 @@
+package neryEnterprises
+
+fun main() {
+    val gabriel = Employee(name = "Gabriel", salary = 4000.0, employment = "PJ")
+    val roberto = Employee(name = "Roberto", salary = 2500.0, employment = "CLT")
+    val miranda = Employee(name = "Miranda", salary = 3300.0, employment = "CLT")
+
+    val management = EmployeesManager<Employee>()
+
+    management.create(gabriel.name, gabriel)
+    management.create(roberto.name, roberto)
+    management.create(miranda.name, miranda)
+
+    println(management.findById(gabriel.name))
+}
